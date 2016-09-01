3a0c2adaf5e25cbbbe53922f57560cf0bd308ec4
Misi <Misi@DEVELOPER>
2016-09-01 09:45:13 +0200

    Ez a próba project első commit-ja

---
+++ proba/proba.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   proba.cpp
+ * Author: Misi
+ *
+ * Created on 2016. szeptember 1., 9:42
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
